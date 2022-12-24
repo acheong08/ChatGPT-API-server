5200e5fccbc5106359b55908b8819eaed6098852
SadMap <71943073+SadMap@users.noreply.github.com>
2022-12-24 21:17:46 +0700

    fix

--- handlers/api.go
+++ handlers/api.go
@@ -13,7 +13,7 @@ import (
 // // # API routes
 func API_ask(c *gin.Context) {
 	println(c.Request.Header.Get("Authorization"), os.Args[2])
-	if c.Request.Header.Get("Authorization") != os.Args[2] {
+	if c.Request.Header["Authorization"][0] != os.Args[2] {
 		c.JSON(401, gin.H{
 			"error": "Invalid API key",
 		})
